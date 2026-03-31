ccfe6fb7af3d396f82d9fd79662e925b474626c5
Giacomo Baso <gbaso@users.noreply.github.com>
2026-03-31 10:54:45 +0200

    add application

---
+++ src/test/java/com/github/gbaso/ppb/PathpatternbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gbaso.ppb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathpatternbuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
